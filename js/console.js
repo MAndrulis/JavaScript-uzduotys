5d87e7600e3ddbf03afb397150fe986673ca2ae7
Modestas And <modestas.andrulis@gmail.com>
2023-06-02 09:55:55 +0200

    console

---
+++ js/console.js
@@ -0,0 +1,45 @@
+
+
+// Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+// Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+
+// 1
+const a = 6;
+const b = 7;
+const c = 8;
+console.log(a, b, c);
+
+// 2
+
+const zodis1 = 'Audi';
+const zodis2 = 'Juoda';
+const zodis3 = 'Kava';
+console.log(zodis1);
+console.log(zodis2);
+console.log(zodis3);
+
+// 3
+
+const skaiciai = [1, 2, 3, 4, 5];
+const skaiciai1= [10, 11, 12, 13, 14];
+const skaiciai2 = [21, 22, 23, 24, 25];
+
+console.log(skaiciai,skaiciai1, skaiciai2,);
+
+console.clear();
+
+// 4
+
+const zodziai1 = ['Jonas', 'Antanas', 'Ona', 'Maryte', 'Algis'];
+const zodziai2 = ['Spurga', 'Duona', 'Batonas', 'Bandelė', 'Kibinas'];
+const zodziai3 = ['Agrastas', 'Braskes', 'Serbentai', 'Melynes', 'Avietes'];
+
+console.log(zodziai1);
+console.log(zodziai2);
+console.log(zodziai3);
